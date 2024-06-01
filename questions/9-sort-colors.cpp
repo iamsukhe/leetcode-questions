57614fffb3a32a0112f8cf958e2295850b6156e8
Sukhvinder Singh <79562397+iamsukhe@users.noreply.github.com>
2024-06-01 13:02:32 +0530

    feat: added sort-colors

---
+++ questions/9-sort-colors.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// https://leetcode.com/problems/sort-colors/
+int main() {
+  vector<int> nums = {2, 0, 2, 1, 1, 0};
+
+  int i = 0;
+  int j = nums.size() - 1;
+
+  int index = 0;
+
+  while (index <= j) {
+    if (nums[index] == 0) {
+      swap(nums[index], nums[i]);
+      i++;
+      index++;
+    }
+
+    else if (nums[index] == 2) {
+      swap(nums[index], nums[j]);
+      j--;
+    } else {
+      index++;
+    }
+  }
+
+  for (int k = 0; k < nums.size(); k++) {
+    cout << nums[k] << " ";
+  }
+
+  return 0;
+}
