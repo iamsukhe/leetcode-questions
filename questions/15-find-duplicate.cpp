9083c60e762e948a7ce47f98c88c9a06153146a6
Sukhvinder Singh <79562397+iamsukhe@users.noreply.github.com>
2024-06-02 15:34:24 +0530

    feat: findDuplicate

---
+++ questions/15-find-duplicate.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findDuplicate(vector<int> &nums) {
+
+  // Negative marking solution
+  // int ans = -1;
+
+  // for (int i = 0; i < nums.size(); i++)
+  // {
+  //     if (nums[abs(nums[i])] < 0)
+  //     {
+  //         ans = abs(nums[i]);
+  //         break;
+  //     }
+  //     nums[abs(nums[i])] = -nums[abs(nums[i])];
+  // }
+
+  // return ans;
+
+  // Positioning Mathod
+  while (nums[0] != nums[nums[0]]) {
+    swap(nums[0], nums[nums[0]]);
+  }
+
+  return nums[0];
+}
+
+int main() {
+  vector<int> nums = {3, 3, 3, 3, 3};
+
+  int ans = findDuplicate(nums);
+
+  cout << ans << " ";
+  return 0;
+}
